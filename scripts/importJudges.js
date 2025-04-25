12d4f3393466268d050c4fb262b6829d547b00b0
AlvaroDSolari19 <AlvaroDSolari19@outlook.com>
2025-04-25 03:59:28 -0400

    Added Script to Add Judges to DB

---
+++ scripts/importJudges.js
@@ -0,0 +1,49 @@
+const XLSX = require('xlsx'); 
+const { MongoClient } = require('mongodb'); 
+require('dotenv').config(); 
+
+/* GET COMMAND LINE ARGUMENTS*/
+const filePath = process.argv[2]; 
+const currentLanguage = process.argv[3];
+
+if (!filePath || !currentLanguage){
+    console.error(`Usage: node scripts/importJudges.js <path_to_file.xlsx> <language_code>`);
+    process.exit(1);
+}
+
+async function main() {
+    const client = new MongoClient(process.env.MONGODB_URI); 
+
+    try{
+        await client.connect(); 
+        const db = client.db('IAMOOT-DB');
+        const collection = db.collection('judges'); 
+
+        /* READ EXCEEL FILES */
+        const workbook = XLSX.readFile(filePath); 
+        const sheetName = workbook.SheetNames[0];
+        const rawData = XLSX.utils.sheet_to_json(workbook.Sheets[sheetName]); 
+        
+        /* TRANSFORM DATA */
+        const allJudges = rawData.map( currentRow => ({
+            fullName: currentRow['fullName'], 
+            primaryEmail: currentRow['primaryEmail'], 
+            secondaryEmail: currentRow['secondaryEmail'], 
+            oralRounds: currentRow['oralRounds'], 
+            currentLanguage: currentLanguage
+        }));
+
+        /* PASSWORD GENERATOR */
+
+        /* INSERT INTO MONGODB */
+        const finalResult = await collection.insertMany(allJudges); 
+        console.log(`Succesfully inserted ${finalResult.insertedCount} judges`);
+        
+    } catch (err){
+        console.error('Error importing judges: ', err); 
+    } finally {
+        await client.close(); 
+    }
+}
+
+main();
