45c11c816a51e0dad34ce6601850163b105ec4f6
Alvaro Solari <AlvaroDSolari19@outlook.com>
2025-05-17 04:20:35 -0400

    Sorted Teams for Admin by Wins and Memorandum Average Score

--- routes/adminTeams.js
+++ routes/adminTeams.js
@@ -6,9 +6,11 @@ router.get('/admin/teams', async (req, res) => {
     
     try {
         const teamsCollection = getCollection('teams'); 
-        const allTeams = await teamsCollection.find({}).toArray(); 
-        //Sorting logic: In this case, we need to sort by number of victories and then by memorandum score
-        res.json(allTeams); 
+        const sortedTeams = await teamsCollection
+            .find({})
+            .sort({ preliminaryWins: -1, averageMemoScore: -1})
+            .toArray(); 
+        res.json(sortedTeams); 
     } catch (err) {
         console.error('Error fetching teams: ', err);
         res.status(500).json({ error: 'Internal server error'}); 
